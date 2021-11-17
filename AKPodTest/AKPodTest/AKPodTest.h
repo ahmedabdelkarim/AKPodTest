70e6f601a1f74134178b7b31da0c329a1bb42cff
Ahmed Abdelkarim <ahmed.abdelkarim@itworx.com>
2021-11-17 18:20:01 +0200

    Added framework

---
+++ AKPodTest/AKPodTest/AKPodTest.h
@@ -0,0 +1,18 @@
+//
+//  AKPodTest.h
+//  AKPodTest
+//
+//  Created by Ahmed Abdelkarim on 17/11/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AKPodTest.
+FOUNDATION_EXPORT double AKPodTestVersionNumber;
+
+//! Project version string for AKPodTest.
+FOUNDATION_EXPORT const unsigned char AKPodTestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AKPodTest/PublicHeader.h>
+
+
---
+++ AKPodTest/AKPodTest/PodClasses/Test.swift
@@ -0,0 +1,14 @@
+//
+//  Test.swift
+//  AKPodTest
+//
+//  Created by Ahmed Abdelkarim on 17/11/2021.
+//
+
+import Foundation
+
+public class Test {
+    public func test() {
+        print("test..")
+    }
+}
